e2fae093f8fa87a90233ca5093306e03028cb744
joraeng <60039286+joraeng@users.noreply.github.com>
2021-08-24 14:13:29 +0900

    Update UploadPage select option

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typography, Button, Form, Input } from "antd";
 
-const { Title } = Typography;
 const { TextArea } = Input;
 
+// select option 데이터
+const Contients = [
+	{ key: 1, value: "아프리카" },
+	{ key: 2, value: "유럽" },
+	{ key: 3, value: "아시아" },
+	{ key: 4, value: "북아메리카" },
+	{ key: 5, value: "남아메리카" },
+	{ key: 6, value: "오스트레일리아" },
+	{ key: 7, value: "남극" },
+];
+
 function UploadProductPage() {
+	const [Title, setTitle] = useState("");
+	const [Description, setDescription] = useState("");
+	const [Price, setPrice] = useState("");
+	const [Continent, setContinent] = useState(1);
+	const [image, setimage] = useState([]);
+
+	const titleChangeHandler = (event) => {
+		setTitle(event.currentTarget.value);
+	};
+	const descriptionChangeHandler = (event) => {
+		setDescription(event.currentTarget.value);
+	};
+	const priceChangeHandler = (event) => {
+		setPrice(event.currentTarget.value);
+	};
+	const continentChangeHandler = (event) => {
+		setContinent(event.currentTarget.value);
+	};
+
 	return (
 		<div style={{ maxWidth: "700px", margin: "2rem auto" }}>
 			<div style={{ textAlign: "center", marginBottom: "2rem" }}>
-				<Title level={2}>상품 업로드</Title>
+				<h2>상품 업로드</h2>
 			</div>
 
 			<Form>
@@ -16,19 +45,25 @@ function UploadProductPage() {
 				<br />
 				<br />
 				<label>이름</label>
-				<Input />
+				<Input value={Title} onChange={titleChangeHandler} />
 				<br />
 				<br />
 				<label>설명</label>
-				<TextArea />
+				<TextArea value={Description} onChange={descriptionChangeHandler} />
 				<br />
 				<br />
 				<label>가격($)</label>
-				<Input />
+				<Input type="number" value={Price} onChange={priceChangeHandler} />
 				<br />
 				<br />
-				<select>
-					<option></option>
+				<select onChange={continentChangeHandler} value={Continent}>
+					{Contients.map((item) => {
+						return (
+							<option key={item.key} value={item.key}>
+								{item.value}
+							</option>
+						);
+					})}
 				</select>
 				<br />
 				<br />
